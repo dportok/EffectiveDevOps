9b196078c7ee3c79e9e60be3b8599014e6ca6094
dportok <dportok@gmail.com>
2018-07-28 20:53:19 +0100

    Add jenkins role

---
+++ chapter3/jenkins-cf-template.py
@@ -0,0 +1,132 @@
+"""Generating CloudFormation template."""
+from ipaddress import ip_network
+from ipify import get_ip
+from troposphere import (
+    Base64,
+    ec2,
+    GetAtt,
+    Join,
+    Output,
+    Parameter,
+    Ref,
+    Template,
+)
+
+from troposphere.iam import (
+    InstanceProfile,
+    PolicyType as IAMPolicy,
+    Role,
+)
+
+from awacs.aws import (
+    Action,
+    Allow,
+    Policy,
+    Principal,
+    Statement,
+)
+
+from awacs.sts import AssumeRole
+
+ApplicationName = "jenkins"
+ApplicationPort = "8080"
+
+GithubAccount = "dportok"
+GithubAnsibleURL = "https://github.com/{}/EffectiveDevOps".format(GithubAccount)
+
+AnsiblePullCmd = \
+    "/usr/local/bin/ansible-pull -U {} {}.yaml -i localhost".format(
+        GithubAnsibleURL,
+        ApplicationName
+    )
+
+PublicCidrIp = str(ip_network(get_ip()))
+
+t = Template()
+
+t.add_description("Effective DevOps in AWS: HelloWorld web application")
+
+t.add_parameter(Parameter(
+    "awstest",
+    Description="Name of an existing EC2 KeyPair to SSH",
+    Type="AWS::EC2::KeyPair::KeyName",
+    ConstraintDescription="must be the name of an existing EC2 KeyPair.",
+))
+
+t.add_resource(ec2.SecurityGroup(
+    "SecurityGroup",
+    GroupDescription="Allow SSH and TCP/{} access".format(ApplicationPort),
+    SecurityGroupIngress=[
+        ec2.SecurityGroupRule(
+            IpProtocol="tcp",
+            FromPort="22",
+            ToPort="22",
+            CidrIp=PublicCidrIp,
+        ),
+        ec2.SecurityGroupRule(
+            IpProtocol="tcp",
+            FromPort=ApplicationPort,
+            ToPort=ApplicationPort,
+            CidrIp="0.0.0.0/0",
+        ),
+    ],
+))
+
+ud = Base64(Join('\n', [
+    "#!/bin/bash",
+    "sudo yum install --enablerepo=epel -y git",
+    "sudo yum -y install python34",
+    "sudo wget https://bootstrap.pypa.io/get-pip.py -O /home/ec2-user/get-pip.py",
+    "sudo python3 /home/ec2-user/get-pip.py --user",
+    "sudo pip install --upgrade pip",
+    "sudo /usr/local/bin/pip install --upgrade setuptools",
+    "sudo /usr/local/bin/pip install ansible",
+    AnsiblePullCmd,
+    "sudo echo '*/10 * * * * ec2-user {}' > /etc/cron.d/ansible-pull".format(AnsiblePullCmd)
+]))
+
+t.add_resource(Role(
+    "Role",
+    AssumeRolePolicyDocument=Policy(
+        Statement=[
+            Statement(
+                Effect=Allow,
+                Action=[AssumeRole],
+                Principal=Principal("Service", ["ec2.amazonaws.com"])
+            )
+        ]
+    )
+))
+
+t.add_resource(InstanceProfile(
+    "InstanceProfile",
+    Path="/",
+    Roles=[Ref("Role")]
+))
+
+t.add_resource(ec2.Instance(
+    "instance",
+    ImageId="ami-a4c7edb2",
+    InstanceType="t2.micro",
+    SecurityGroups=[Ref("SecurityGroup")],
+    KeyName=Ref("awstest"),
+    UserData=ud,
+    IamInstanceProfile=Ref("InstanceProfile"),
+))
+
+t.add_output(Output(
+    "InstancePublicIp",
+    Description="Public IP of our instance.",
+    Value=GetAtt("instance", "PublicIp"),
+))
+
+t.add_output(Output(
+    "WebUrl",
+    Description="Application endpoint",
+    Value=Join("", [
+        "http://", GetAtt("instance", "PublicDnsName"),
+        ":", ApplicationPort
+    ]),
+))
+
+print(t.to_json())
